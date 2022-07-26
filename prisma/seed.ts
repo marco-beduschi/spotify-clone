080e358a819164b9d9ea23e9041b0eda54add521
Marco Beduschi <8469440+marco-beduschi@users.noreply.github.com>
2022-07-26 10:15:44 -0300

    feat: add /api/signup route

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 import { artistsData } from "./songsData";
 
 const prisma = new PrismaClient();
@@ -17,6 +18,35 @@ const run = async () => {
               duration: song.duration,
               url: song.url,
             })),
+          },
+        },
+      });
+    })
+  );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: {
+      email: "user@test.com",
+    },
+    update: {},
+    create: {
+      email: "user@test.com",
+      password: bcrypt.hashSync("anyPassword", salt),
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(0).map((_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist ${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({ id: song.id })),
           },
         },
       });
